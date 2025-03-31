91f1387f20d3b4e122fd3232a29cf05ea62aa591
Kamini Negi <kamini893@gmail.com>
2025-03-31 01:48:48 +0100

    updated pom

---
+++ src/test/java/com/spring/security/test/SpringApplicationTests.java
@@ -0,0 +1,14 @@
+package com.spring.security.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Just verifies Spring context starts correctly
+	}
+
+}
